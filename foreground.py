98bcec94f0ae443cc613129e66d1d6f08fff1464
eitan.b09@gmail.com <eitan.b09@gmil.com>
2023-10-30 18:30:10 +0000

    optimized forground by daniel

--- foreground.py
+++ foreground.py
@@ -46,25 +46,34 @@ class PESMODForegroundEstimation():
     def __call__(self, frame):
         if self.frames_history is None:
             self.frames_history = np.expand_dims(frame,axis=0)
+            self.window_sum = self.frames_history[-1].astype(np.int32)
             return frame
-        
-        else:
 
+        else:
             filter_w, filter_h = self.neighborhood_matrix
             pad_w = int(filter_w / 2)
             pad_h = int(filter_h / 2)
 
-            background = np.mean(self.frames_history, axis=0)
+            background = self.window_sum / len(self.frames_history)
             padded_background = np.pad(background, ((pad_w, pad_w), (pad_h, pad_h)), constant_values=(np.inf, np.inf))
             background_patches = view_as_windows(padded_background, self.neighborhood_matrix)
 
             w, h = frame.shape
-            foreground = np.abs(background_patches.reshape(-1) - np.repeat(frame, filter_w * filter_h)).reshape(w, h, -1).min(axis=2).astype(np.uint8)
+            
+            #foreground = np.abs(background_patches.reshape(-1) - np.repeat(frame, filter_w * filter_h)).reshape(w, h, -1).min(axis=2).astype(np.uint8)
+
+            foreground = np.abs(np.transpose(background_patches, (2, 3, 0, 1)) - 
+                               np.broadcast_to(frame, (filter_w, filter_h, w, h))).transpose((2, 3, 0 ,1)).reshape(w, h, -1).min(axis=2).astype(np.uint8)
+
 
             self.frames_history = np.append(self.frames_history,np.expand_dims(frame, axis=0), axis=0)
-            
+
             if self.frames_history.shape[0]> self.num_frames:
-                self.frames_history = self.frames_history[-self.num_frames:]
+                self.window_sum -= self.frames_history[0]
+                self.frames_history = self.frames_history[1:]
+
+            self.window_sum += self.frames_history[-1]
+
             return foreground
 
 
