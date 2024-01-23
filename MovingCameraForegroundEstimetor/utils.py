e8a6abe08858e8bb16ee4b3643680267ecd1fdbf
sharon komissarov <sharon200102>
2024-01-23 14:38:59 +0000

    first version of decorator that will analyze the rotation

--- MovingCameraForegroundEstimetor/utils.py
+++ MovingCameraForegroundEstimetor/utils.py
@@ -1,6 +1,7 @@
 import numpy as np
-
-
+import cv2 as cv
+import matplotlib.pyplot as plt
+from pathlib import Path
 def get_grid_coords(num_grids_x, num_grids_y):
     """
     get coordinates of the grids, the first grid is (0, 0), then (1, 0) and so on
@@ -30,3 +31,36 @@ def project(points, H):
 
 def reshape(arr: np.ndarray, new_shape):
     return arr.reshape(*new_shape)
+
+
+class PlotFramesDecorator:
+    def __init__(self, save_dir):
+        self.counter = 0
+        self.save_dir = Path(save_dir)
+    def __call__(self, func):
+        def wrapper(*args, **kwargs):
+            klt_obj = args[0]
+            previous_gray_frame = klt_obj.imgPrevGray.copy()
+        
+            result = func(*args, **kwargs)
+            self.counter+=1
+            if previous_gray_frame is not None:
+            # Access frames and homography
+                current_gray_frame = klt_obj.imgPrevGray
+                homography = result
+
+                # Apply homography and prepare plotting
+                cv.warpPerspective(current_gray_frame, homography, (previous_gray_frame.shape[1], previous_gray_frame.shape[0]))
+                fig, axes = plt.subplots(1, 2, figsize=(10, 6))
+
+                # Plot frames
+                axes[0].imshow(previous_gray_frame)
+                axes[0].set_title("Previous")
+                axes[1].imshow(current_gray_frame)
+                axes[1].set_title("Rotated Current")
+
+                fig.savefig(self.save_dir/f'{self.counter}_{self.counter+1}.png')
+                
+            return result
+
+        return wrapper
