2cfb53bc7e8fed1891e4b805a181f23b0d861be5
sharon komissarov <sharon200102>
2024-02-12 14:39:38 +0000

    first version of implementation of sol into VMD

--- foreground.py
+++ foreground.py
@@ -5,7 +5,7 @@ from VMD.MovingCameraForegroundEstimetor.ForegroundEstimetor import ForegroundEs
 from time import time
 from numba import jit, prange
 from collections import deque
-
+import torch
 foreground_estimators = {}
 
 
@@ -131,3 +131,127 @@ def difference(background_patches, broadcasted_frame):
             min_diff = np.min(diff)
             foreground[i, j] = min_diff
     return foreground
+
+
+@register("MGDForegroundEstimation")
+class MGDForegroundEstimation():
+    def __init__(self, kernel_shape=9):
+        assert kernel_shape % 2 != 0, ValueError("The kernel should be odd")
+        self.kernel_shape = kernel_shape
+        self._kernel_shape_with_bg = kernel_shape + 2
+        self.n_rings = kernel_shape // 2
+        self.r = np.arange(1, self.n_rings)
+
+        distance_from_center = list(range(1,(self._kernel_shape_with_bg//2)+1))
+        self._X_distances = np.array([distance_from_center[::-1] + [0] + distance_from_center] * self._kernel_shape_with_bg)
+        self._Y_distances = self._X_distances.transpose()
+        self._distance_kernel = np.round(np.sqrt((self._X_distances)**2 + (self._Y_distances)**2))
+        self.ring_kernels = np.stack([self.build_ring_kernel(self._distance_kernel,i) for i in range(self.n_rings)],axis=0)
+        self.backgroung_kernel = self.build_ring_kernel(self._distance_kernel, self.n_rings+1)
+
+        self._X_distances = self._X_distances[1:-1, 1:-1]
+        self._Y_distances = self._Y_distances[1:-1, 1:-1]
+    
+    @staticmethod
+    def build_ring_kernel(distance_kernel:np.array,requested_distance_from_center:int) -> np.array:
+        """
+        Create the ring convolution kernels as descrined in the article.
+        set of connected pixels of the same distance.
+        """  
+        ring = np.zeros(distance_kernel.shape)
+        ring[np.where(distance_kernel == requested_distance_from_center) ] = 1
+        ring /= np.sum(ring)
+        return ring
+    
+    @staticmethod
+    def initial_difference_matrix(mu_matrices:np.array):
+        shifted_mu_matrices = np.roll(mu_matrices,-1,axis=0) # [ring1,ring2,ring3,...,] -> [ring2,ring3,...,ring1]    
+        mu_matrices[-1,:,:] = 0 # zero the last ring because its irrelevant to follwoing the substraction
+        shifted_mu_matrices[-1,:,:] = 0 # zero the first ring because its irrelevant to the follwoing substraciton
+        D = np.sum((np.maximum(mu_matrices - shifted_mu_matrices,0))**2,axis=0)
+        return D
+    
+    @staticmethod
+    def convolve_with_multiple_filters(gray_frame:np.array,filters:np.array,**kwargs):
+        """
+        gray_frame is expected to be an array of shape (height,width)
+
+        filters are expected to be of shape (n_filters,kernel_height,kernel_width)
+
+        """
+        gray_frame = torch.tensor(gray_frame,dtype=torch.float32).view(1,1,*gray_frame.shape) # convert into (1,1,height,width)->(minibatch,channels,height,width)    
+        filters = torch.tensor(filters, dtype=torch.float32).unsqueeze(1) # convert into (n_filters,1,kernel_height,kernel_width)->(out_channels,in_channels,kernel_height,kernel_width)
+        output = torch.nn.functional.conv2d(gray_frame, filters,**kwargs).squeeze(0)
+        return output.numpy() # return a numpy array as output
+    
+    def calculate_sigma(self, mu_matrices, B):
+        P_r = (np.subtract(mu_matrices[1:], B))/(mu_matrices[0] - B) # p(r)-B/p(0)/B, 1<=r<=n_rings
+        sigma_r = self.r/(np.sqrt(-2*np.log(P_r))) # refer algorithm 2 in the article to understand the line
+        sigma_r[np.isnan(sigma_r)] = np.inf
+        sigma_r[P_r == 0] = np.inf # if P_r equals zero it means that the ring is similar to the background and hence the corresponding sigma should be considered.
+        return np.min(sigma_r,axis=0)
+    
+    def calculate_hessian_filter(self, sigma):
+        exponent_power = np.exp(-(self._X_distances**2 + self._Y_distances**2)/(2*(sigma**2)))
+        gxx_coefficient = (-1/(2*np.pi*(sigma**4))) * (1-((self._X_distances**2)/(sigma**2)))
+        gyy_coefficient = (-1/(2*np.pi*(sigma**4))) * (1-((self._Y_distances**2)/(sigma**2)))
+        gxy_coefficient = self._X_distances*self._Y_distances/(2*np.pi*(sigma**6))
+
+        gxx = gxx_coefficient*exponent_power
+        gyy = gyy_coefficient*exponent_power
+        gxy = gxy_coefficient*exponent_power
+        return np.array([gxx,gxy,gxy,gyy])
+    
+    @staticmethod
+    def calculate_ratio_between_eiganvalues(ev1,ev2):
+        ev1, ev2 = np.abs(ev1), np.abs(ev2)
+        return min(ev1, ev2)/max(ev1, ev2)
+    
+    @staticmethod
+    def calculate_hessian_eigenvalues(hessian_matrix):
+        fxx, fxy, fyx, fyy = hessian_matrix.flatten()
+        assert fxy == fyx, ValueError(f"Hessian matrix should be symmetric: {hessian_matrix}")
+        diag_sum = fxx + fyy
+        sqr_root = np.sqrt((fxx - fyy)**2 + 4*fxy*fyx)
+        ev1 = (diag_sum + sqr_root)/2
+        ev2 = (diag_sum - sqr_root)/2
+        return ev1, ev2
+
+    def __call__(self,gray_frame):
+        mu_matrices = self.convolve_with_multiple_filters(gray_frame,self.ring_kernels,padding='same')
+        B = self.convolve_with_multiple_filters(gray_frame,np.expand_dims(self.backgroung_kernel,axis=0),padding='same') # convert background_kernel into (1,kernel_height,kernel_width)
+        D = self.initial_difference_matrix(mu_matrices)
+
+        first_threshold, _ = cv.threshold(D.astype(np.uint16), 0.0, np.max(D), cv.THRESH_OTSU+cv.THRESH_BINARY)
+        second_threshold, _ = cv.threshold(D[D > first_threshold].astype(np.uint16), 0.0, np.max(D), cv.THRESH_OTSU+cv.THRESH_BINARY)
+        
+        # zero all indices that doesnt surpass the threshold
+        corrected_D = D.copy()
+        corrected_D[corrected_D<=second_threshold] = 0
+
+        number_of_points_from_filter_center = self.kernel_shape//2
+        # zero the boundries
+        boundries_map = np.ones(corrected_D.shape)
+        boundries_map[number_of_points_from_filter_center:-number_of_points_from_filter_center,
+                      number_of_points_from_filter_center:-number_of_points_from_filter_center] = 0
+        
+        corrected_D[boundries_map==1] = 0
+        
+        # Check the istropy for the points that surpassed the threshold
+        for col, row in np.argwhere(corrected_D>second_threshold):
+            sigma = self.calculate_sigma(mu_matrices[:, col, row], B[0, col, row])
+            if sigma < np.inf:
+                neighborhood = gray_frame[col-number_of_points_from_filter_center:col+number_of_points_from_filter_center+1,row-number_of_points_from_filter_center:row+number_of_points_from_filter_center+1]            
+
+                hessian_filter = self.calculate_hessian_filter(sigma)                
+                hessian_matrix = np.sum(neighborhood*hessian_filter,axis=(1,2)).reshape(2,2)
+                ev1,ev2 = self.calculate_hessian_eigenvalues(hessian_matrix)
+                
+                if ev1<0 and ev2<0:
+                    I = self.calculate_ratio_between_eiganvalues(ev1,ev2)
+                    corrected_D[col,row] *= I
+                else:
+                    corrected_D[col,row] = 0
+        
+        corrected_D[corrected_D>second_threshold] = 255
+        return corrected_D
